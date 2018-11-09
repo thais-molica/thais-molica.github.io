e67241013efd3292cfac2ba32bb6734e8cea2f0d
Thais Molica Faria <thaisfaria@cvc.com.br>
2018-11-09 00:08:53 -0200

    read me, placehol

--- thais-molica/src/App.vue
+++ thais-molica/src/App.vue
@@ -1,20 +1,24 @@
-<template>
-  <div class="default-container" id="app">
-    <Header/>
-    <About/>
-  </div>
-</template>
-
 <script>
   import Header from "./components/Header/index.vue";
   import About from './components/About/index.vue';
+  import Projects from './components/Projects/index.vue';
   import './assets/default.scss'
 
   export default {
     name: "app",
     components: {
+      // eslint-disable-next-line
       Header,
-      About
+      About,
+      Projects
     }
   };
 </script>
+
+<template>
+  <div class="default-container" id="app">
+    <Header/>
+    <About/>
+    <Projects/>
+  </div>
+</template>
---
+++ thais-molica/src/components/Projects/index.vue
@@ -0,0 +1,16 @@
+
+<script>
+    import './style.scss';
+
+  export default {
+    name: "Projects"
+  };
+</script>
+
+<template>
+    <div class="projects" id="projects">
+        <h2>Projetos</h2>
+        <ul class="project-list">
+        </ul>
+    </div>
+</template>
