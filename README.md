2a7baee90938f79dbc3630225bd106c5ea346c57
Thais Molica Faria <thaisfaria@cvc.com.br>
2018-11-12 14:31:08 -0200

    projetos

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Thaís Molica - Front-end Developer
+
+##TODO
+1. [importação automática dos arquivos sass] https://cli.vuejs.org/guide/css.html#automatic-imports
+2. [router] https://vuejs.org/v2/guide/routing.html
+3. erro: Module Warning (from C:/Users/mtzcpd1124/AppData/Roaming/npm/node_modules/@vue/cli-service-global/node_modules/eslint-loader/index.js):
+error: The "Header" component has been registered but not used (vue/no-unused-components) at App.vue:10:7:
+4. opção em inglês
--- thais-molica/src/components/Projects/index.vue
+++ thais-molica/src/components/Projects/index.vue
@@ -1,16 +1,78 @@
 
 <script>
     import './style.scss';
+    
 
   export default {
-    name: "Projects"
+    name: "Projects",
+    data() {
+        return {
+            projects: [
+                {
+                    name: "Livelo",
+                    photo: "http://thais-molica.github.io/img/livelo.png"
+                },
+                {
+                    name: "EnterPlay",
+                    photo: "http://thais-molica.github.io/img/enterplay.png"
+                },
+                {
+                    name: "Escolha a escola",
+                    photo: "http://thais-molica.github.io/img/escolha_a_escola.png"
+                },
+                {
+                    name: "Eleições 2014",
+                    photo: "http://thais-molica.github.io/img/eleicoes_2014.png"
+                },
+                {
+                    name: "Cvc",
+                    photo: "http://thais-molica.github.io/img/cvc.png"
+                },
+                {
+                    name: "corinthians",
+                    photo: "http://thais-molica.github.io/img/corinthians.png"
+                },
+                {
+                    name: "Partiu",
+                    photo: "http://thais-molica.github.io/img/partiu.png"
+                },
+                {
+                    name: "Viaja Dotz",
+                    photo: "http://thais-molica.github.io/img/dotz.png"
+                },
+                {
+                    name: "Santander",
+                    photo: "http://thais-molica.github.io/img/brasilct.png"
+                },
+                {
+                    name: "Viagem Listo",
+                    photo: "http://thais-molica.github.io/img/viagemlisto.png"
+                },
+                {
+                    name: "Viajanet",
+                    photo: "http://thais-molica.github.io/img/viajanet.png"
+                },
+                {
+                    name: "Corporate Go",
+                    photo: "http://thais-molica.github.io/img/corporatego.png"
+                },
+                {
+                    name: "Travel Agent",
+                    photo: "http://thais-molica.github.io/img/travelagent.png"
+                }
+            ]
+        }
+    }
   };
 </script>
 
 <template>
     <div class="projects" id="projects">
         <h2>Projetos</h2>
-        <ul class="project-list">
+        <ul class="projects-list">
+            <li class="projects-item" v-for="(project, index) in projects" v-bind:key="index">
+                <img class="projects-img" :src="project.photo" :key="project.name" :alt="project.name">
+            </li>
         </ul>
     </div>
 </template>
